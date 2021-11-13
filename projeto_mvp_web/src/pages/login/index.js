69dc684621f9fd46178842eae479fb0fe7f714a3
Carlos Bionde <83780378+chbionde@users.noreply.github.com>
2021-11-13 18:20:58 -0300

    update
    Co-Authored-By: Matheus Saldanha Coelho <54903969+MSaldanha15@users.noreply.github.com>

--- projeto_mvp_web/src/pages/login/index.js
+++ projeto_mvp_web/src/pages/login/index.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React,{useContext,useState} from "react";
+
+import {UsuarioContext,} from '../../context/user';
 
 function Login() {
+
+    const {signIn, signUp} = useContext(UsuarioContext);
+    const [email,setEmail] = useState('teste@gmail.com');
+    const [password,setpassword] = useState('123456');
+    
+    const handleLogin = async () => {
+        try{
+            await signIn(email,password);
+        }catch(err){
+            console.warn(err);
+        }
+    }
+
+    const handleCadastro = async () => {
+        try{
+            await signUp(email,password);
+        }catch(err){
+            console.warn(err);
+        }
+    }
     return (
         <div>
             <h1>
                 Login
             </h1>
+            <button type="button" onClick={handleLogin}>
+                Logar
+            </button>
+            <button type="button" onClick={handleCadastro}>
+                Cadastrar
+            </button>
         </div>
     )
 }
